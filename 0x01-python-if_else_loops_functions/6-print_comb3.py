461c47d2a6e8ef0093abfaefc42d3fa661f32cd7
Aymane <aymane.lamarti.8@gmail.com>
2024-01-03 17:37:47 +0000

    combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,9 +3,9 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if (i != j and i < j):
-            print(f"{i}{j}", end='')
+            print("{}{}".format(i, j), end='')
             if (i + j != 17):
-                print(", ", end='')
+                print(", ".format(), end='')
         j += j
     i += i
-print("")
+print("".format())
