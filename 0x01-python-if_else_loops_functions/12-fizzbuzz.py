3527884f7858a05039fea079c059fa86311aae39
Aymane <aymane.lamarti.8@gmail.com>
2024-01-03 18:41:39 +0000

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for i in range(1, 101, 1):
+        if (i % 3 == 0) and !(i % 5 == 0):
+            print("Fizz", end='');
+        elif (i % 5 == 0) and !(i % 3 == 0):
+            print("Buzz", end='')
+        elif (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz", end='')
+        else:
+            print(i, end='')
+
+        if (i != 100):
+            print(" ", end='')
+        else:
+            print("")
+
+        return 0
