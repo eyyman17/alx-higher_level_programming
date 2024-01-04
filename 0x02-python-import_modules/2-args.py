6b38ee43420113c8053592773a5610dac2578e18
Aymane <aymane.lamarti.8@gmail.com>
2024-01-04 21:13:19 +0000

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print(f"{argc} arguments:")
-    for i in range(1, argc + 1):
-        print(f"{i}: {sys.argv[i]}")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
