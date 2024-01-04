05d653b353a62538c555bf0b7670cf75889d187d
Aymane <aymane.lamarti.8@gmail.com>
2024-01-04 21:08:09 +0000

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-argc = len(sys.argv) - 1
-
-if argc == 0:
-    print("0 arguments.")
-else:
-    print(f"{argc} arguments:")
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print(f"{argc} arguments:")
     for i in range(1, argc + 1):
         print(f"{i}: {sys.argv[i]}")
