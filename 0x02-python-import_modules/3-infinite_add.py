b5d0bb8d48aa1e2cbbec1fcc54719cd141a35533
Aymane <aymane.lamarti.8@gmail.com>
2024-01-04 21:31:44 +0000

    python is cool

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argc = len(sys.argv)
+    s = 0
+
+    for i in range(1, argc):
+        s = s + int(sys.argv[i])
+    print(f"{s}")
