bd641e00b8c52b02a7a6b4dd46ccf547b2934c3e
Aymane <aymane.lamarti.8@gmail.com>
2024-01-05 00:04:26 +0000

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,7 +12,11 @@ if __name__ == "__main__":
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    if sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    if sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    if sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
