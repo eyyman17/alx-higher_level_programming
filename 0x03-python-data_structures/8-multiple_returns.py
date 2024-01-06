df3058f8ab52788b46700727f732329253e5bf4b
Aymane <aymane.lamarti.8@gmail.com>
2024-01-06 15:54:01 +0000

    daata structures

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0]
+    if sentence == "":
+        first_char = None
+    return (length, first_char)
