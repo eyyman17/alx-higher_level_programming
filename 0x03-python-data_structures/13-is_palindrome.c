7f7b8164644d01086b3e1d57023d094cac5227a5
Aymane <aymane.lamarti.8@gmail.com>
2024-01-06 18:27:29 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -35,21 +35,33 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr = *head, *head2;
+	listint_t *ptr = *head, *temp, *head2 = NULL, *new = NULL;
+	int r = 5;
 
+	temp = ptr;
 	while (ptr != NULL)
 	{
-		add_nodeint(&head2, ptr->n);
+		new = add_nodeint(&head2, ptr->n);
+		if (new == NULL)
+		{
+			free_listint(head2);
+			return (0);
+		}
 		ptr = ptr->next;
 	}
-
+	ptr = temp;
 	while (ptr != NULL && head2 != NULL)
 	{
 		if (ptr->n != head2->n)
-			return (0);
+		{
+			r = 0;
+			break;
+		}
+		ptr = ptr->next;
+		head2 = head2->next;
 	}
 
-	free_listint(head2)
+	free_listint(new);
 
-	return (1);
+	return (r);
 }
