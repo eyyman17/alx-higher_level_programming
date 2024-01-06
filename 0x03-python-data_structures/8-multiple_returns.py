7b4eea250d453446a6a1e9cac856aadfc276282c
Aymane <aymane.lamarti.8@gmail.com>
2024-01-06 16:25:15 +0000

    returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    length = len(sentence)
     if sentence == "":
         first_char = None
-    length = len(sentence)
-    first_char = sentence[0]
+    else:
+        first_char = sentence[0]
     return (length, first_char)
