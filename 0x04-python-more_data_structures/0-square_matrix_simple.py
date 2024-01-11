8eb6859de61eef343db1dab5d70504af96449127
Aymane <aymane.lamarti.8@gmail.com>
2024-01-11 00:33:04 +0000

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,11 +3,11 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
     k = 0
-    l = 0
+    t = 0
     for i in matrix:
         for j in i:
-            new_matrix[l][k] = j**2
+            new_matrix[t][k] = j**2
             k += 1
         k = 0
-        l += 1
+        t += 1
     return new_matrix
