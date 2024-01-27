d4f3c87ff9c0cb13507eae16a52b3afe735f9f88
Aymane <aymane.lamarti.8@gmail.com>
2024-01-27 20:26:29 +0000

    set

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,6 +8,10 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
 
     @property
     def size(self):
