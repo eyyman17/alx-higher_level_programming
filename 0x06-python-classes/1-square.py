49ca27663eff7014e23a6f756fb829b3fee4d813
Aymane <aymane.lamarti.8@gmail.com>
2024-01-27 19:45:22 +0000

    squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 1-square module
+Classes:
+    'Square' : This the class of squares.
+"""
+
+
+class Square:
+    """ This os the square class """
+
+    def __init__(self, size):
+        self.__size = size
