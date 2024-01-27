d4d616f7fe8372094bc7a89d50d417858257eeaf
Aymane <aymane.lamarti.8@gmail.com>
2024-01-27 20:24:52 +0000

    set

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,6 +19,7 @@ class Square:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         return self.__size ** 2
