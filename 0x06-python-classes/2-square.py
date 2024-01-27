21a8d1baddfb776d0559721d3783ff088254f5c2
Aymane <aymane.lamarti.8@gmail.com>
2024-01-27 19:51:01 +0000

    python classses

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Size validation 
+Classes:
+    Square: the square class
+"""
+
+
+class Square:
+    """ The Square Class """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
