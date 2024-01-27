9859014b818f4d1eae134c9f8bfe751aaddaf684
Aymane <aymane.lamarti.8@gmail.com>
2024-01-27 20:20:28 +0000

    get set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" 4. Access and update private attribute """
+
+
+class Square:
+    """ The Square class """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
