7feb7803e806883689676cf9ff070d49de3c821b
Aymane <aymane.lamarti.8@gmail.com>
2024-01-29 23:18:10 +0000

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+4. Text indentation
+
+
+"""
+
+def text_indentation(text):
+    """
+     a function that prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    limit = len(text)
+    i = 0
+    if text[len(text) - 1] == ' ':
+        limit -= 1
+    if text[0] == ' ':
+        i += 1
+    while i < limit:
+        if text[i] in ['.', '?', ':']:
+            print(f"{text[i]}\n")
+            if i + 1 < len(text) - 1:
+                if text[i + 1] == ' ':
+                    i += 1
+        else:
+            print(text[i], end="")
+        i += 1
