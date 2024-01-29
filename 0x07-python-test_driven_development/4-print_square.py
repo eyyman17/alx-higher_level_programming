e83a1f127c10f67b0e3fb2adf94fce920f79a655
Aymane <aymane.lamarti.8@gmail.com>
2024-01-29 21:19:14 +0000

    tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+3. Print Square
+
+
+"""
+
+def print_square(size):
+    """
+    a function that prints a square with the character #.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
