ff485cd80e698d4de2f470f8816ab6453e8d5566
Aymane <aymane.lamarti.8@gmail.com>
2024-01-30 00:56:38 +0000

    symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
     """ Rectangle Class """
 
     number_of_instances = 0
-    print_symbol = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
