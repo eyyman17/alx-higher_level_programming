9afe0a44e0df1d2b15cc946de0815ca330c7a48c
Aymane <aymane.lamarti.8@gmail.com>
2024-02-01 17:51:33 +0000

    matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,10 +8,14 @@
 
 
 def matrix_divided(matrix, div):
-    """ Function that divides all elements of a matrix. """
+    """
+    Function that divides all elements of a matrix.
+    """
 
-    if not all(isinstance(item, (int, float)) for item in matrix) and not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(item, (int, float)) for item in matrix)
+    and not isinstance(matrix, list):
+        raise
+        TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
@@ -23,6 +27,7 @@ def matrix_divided(matrix, div):
     for item in matrix:
         row = []
         for element in item:
-            row.append(round(element\div, 2))
+            rounded = round(element\div, 2)
+            row.append(rounded)
         new_matrix.append(row)
     return new_matrix
