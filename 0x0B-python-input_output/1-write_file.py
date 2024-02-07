51f936aebe7b12078b45391f73f2400d931c3e21
Aymane <aymane.lamarti.8@gmail.com>
2024-02-07 01:05:01 +0000

    file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,7 @@ Writing to a file module
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of characters"""
+    """writes a string to a text file (UTF8)"""
     try:
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(text)
