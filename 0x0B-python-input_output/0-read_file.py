74d45d47d5c018c5604d32ec0e6e33f59de80088
Aymane <aymane.lamarti.8@gmail.com>
2024-02-07 00:47:22 +0000

    intput output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Reading a file module
+"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        read_data = f.read()
+        print(read_data, end="")
+
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+read me 
+Python input output
