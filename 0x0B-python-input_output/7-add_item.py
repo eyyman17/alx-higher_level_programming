e1b06a1b7550c8e117d99fed42fc5707c0a6ad43
Aymane <aymane.lamarti.8@gmail.com>
2024-02-11 12:14:57 +0000

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
-import json
+
+""" Load, add, save module """
 import sys
-from pathlib import Path
-"""
-Load, add, save module
-"""
 
 
-def save_to_json_file(my_obj, filename):
-    """ writes an Object to a text file, using a JSON """
-    with open(filename, 'w+') as f:
-        f.write(json.dumps(my_obj))
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def load_from_json_file(filename):
-    """ creates an Object from a “JSON file” """
-    with open(filename, 'r') as f:
-        return json.loads(f.read())
-
-
-def main():
-    """ The main function """
-    path = Path("./add_item.json")
-    if path.is_file():
-        p_list = load_from_json_file("add_item.json")
-        if type(p_list) is not list:
-            save_to_json_file([], "add_item.json")
-            p_list = load_from_json_file("add_item.json")
-    else:
-        save_to_json_file([], "add_item.json")
-    p_list = load_from_json_file("add_item.json")
-    for i in range(1, len(sys.argv)):
-        p_list.append(sys.argv[i])
-        save_to_json_file(p_list, "add_item.json")
-
-
-main()
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
