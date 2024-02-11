41947914814cf7ce73def4a6b7b0e1bb5e76a032
Aymane <aymane.lamarti.8@gmail.com>
2024-02-11 12:19:38 +0000

    permissions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,14 @@
 
 """ Load, add, save module """
 import sys
-
+import os
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    os.chmod(__file__, 0o755)
 
     try:
         items = load_from_json_file("add_item.json")
