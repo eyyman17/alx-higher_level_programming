f1de75d4ae14e7eaf9520b4b6db067d0297248a2
Aymane <aymane.lamarti.8@gmail.com>
2024-02-12 18:39:23 +0000

    list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" My list Module """
+
+
+class MyList:
+    """ MyList class """
+
+    def print_sorted(self):
+        """ print_sorted function """
+        a = sorted(self)
+        print(a)
