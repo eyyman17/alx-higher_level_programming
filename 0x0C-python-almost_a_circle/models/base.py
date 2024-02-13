a0e2f1da9da44b4fb2031adcb4ecc78ef620e7d6
Aymane <aymane.lamarti.8@gmail.com>
2024-02-13 02:17:07 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns JSON string representation """
-        if list_dictionaries is None or len(list_dictionaries) = 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
