f813799707e7a21eae2d8f74485ac9952587727f
Aymane <aymane.lamarti.8@gmail.com>
2024-02-19 23:07:43 +0000

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Same class or inherit from """
+
+
+def is_kind_of_class(obj, a_class):
+    """ is kind of class function """
+    if isinstance(obj, a_class):
+        return True
+    return False
